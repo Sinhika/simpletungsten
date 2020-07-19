0640f28510156860f0a509de2da36a0ddf8cd7cd
Sinhika <sinhika@republicofnewhome.org>
2020-07-19 10:34:52 -0500

    removed oldsrc, no longer needed for reference

--- oldsrc/java/akkamaddi/plugins/simpletungsten/ModInfo.java
+++
@@ -1,8 +0,0 @@
-package akkamaddi.plugins.simpletungsten;
-
-public class ModInfo 
-{
-    public static final String ID = "simpletungsten";
-    public static final String NAME = "Simple Tungsten, and Tungsten alloys";
-    public static final String VERSION = "1.7.10-1.5.5";
-}
